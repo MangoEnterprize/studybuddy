e646676e1e6dfc39c31dfaca492529658fecca73
MangoEnterprise <summerlewinstan@gmail.com>
2024-09-24 23:07:42 -0400

    Django REST Framework

--- studybud/urls.py
+++ studybud/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
